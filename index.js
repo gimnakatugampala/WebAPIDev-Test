e49337c7eb4984704cc416b88ad47f4f06c957dc
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-06-22 10:22:02 +0530

    basic_health_server

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 
+app.use(cors());
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+// -- CHECK SERVER HEALTH
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok', timestamp: new Date().toISOString() });
+});
+
 
 
 // Start the server
