7b808418b6e9160f0f32be36e1b8836f19d554c8
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-07-05 12:25:18 +0530

    project_md

--- router/pings.js
+++ router/pings.js
@@ -8,12 +8,24 @@ const router = express.Router();
 
 
 // ---------------------------------------------------------
-// 5. Stations
+// 5. Pings
 // ---------------------------------------------------------
 
 // GET /pings (Collection)
 router.get('/', (req, res) => {
     res.status(200).json(seedData.pings);
+});
+
+// GET /pings/:pingId (Atomic member)
+router.get('/:pingId', (req, res) => {
+    const id = req.params.pingId;
+    const ping = seedData.pings.find(p => p.id == id);
+    
+    if (ping) {
+        res.status(200).json(ping);
+    } else {
+        res.status(404).json({ error: "Ping not found" });
+    }
 });
 
 
